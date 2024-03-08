5bf40ef726d1be94eb193308b36dc80f6b73679f
UiSalam123 <abdussalamsateemae@gmail.com>
2024-03-08 14:40:11 +0700

    For security

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAj2mDVm72uMMDqnpghKfOk8EWs64civqc",
-  authDomain: "sharebox-a837d.firebaseapp.com",
-  projectId: "sharebox-a837d",
-  storageBucket: "sharebox-a837d.appspot.com",
-  messagingSenderId: "1064256447155",
-  appId: "1:1064256447155:web:22bec44cf32ac2d28b46c6",
-  measurementId: "G-JRYS929REP",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
